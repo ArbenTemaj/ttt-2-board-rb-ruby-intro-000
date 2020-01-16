61a027ab67e97fcf67292a082d3f92616bcd1307
Arben Temaj <arbentemaj@gmail.com>
2020-01-16 20:21:31 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,1 +1,23 @@
-board = ["0", "1", "2", "3", "4", "5", "6", "7", "8"]
+top_board = ["1", "2", "3"]
+
+top_board[0] = position1
+
+top_board[1] = position2
+
+top_board[2] = position3
+
+middle_board = ["4", "5", "6"]
+
+middle_board[0] = position4
+
+middle_board[1] = position5
+
+middle_board[2] = position6
+
+bottom_board = ["7", "8", "9"]
+
+bottom_board[0] = position7
+
+bottom_board[1] = position8
+
+bottom_board[2] = position9
